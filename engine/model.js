43e902ef5fbc63736a5fd76061cd8cd0af767661
wbucher3 <wbucher@live.unc.edu>
2020-10-22 16:00:50 -0400

    messing around with pixi in model

--- engine/model.js
+++ engine/model.js
@@ -1,7 +1,22 @@
 /* handles all the game logic */
+import {Application, Sprite} from './node_modules/pixi.js'
 
 export default class Model {
     constructor() {
+        let player = {
+            sprite= new Sprite.fromImage("../images/player.png"),
+            isDead:false,
+        }
+        let background = {
+            sprite= new Sprite.fromImage("../images/background.png"),
+
+        }
+
+        let obstacles = {
+            sprite= new Sprite.fromImage("../images/obstacle.png")
+
+            
+        }
         
     }
 }
