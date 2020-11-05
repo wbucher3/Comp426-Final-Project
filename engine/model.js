31c05828dcf47a6e582cc5d12075657fe92adc91
wbucher3 <wbucher@live.unc.edu>
2020-11-05 00:34:29 -0500

    add new sprites, lane movement, and landing page loading

--- engine/model.js
+++ engine/model.js
@@ -1,55 +1,49 @@
 /* handles all the game logic */
 
-/*i think the easiest way to make this game work is to have the player remain stationary
-and the obstacles move toward the player. then the player can either duck or jump
-honestly this is super similar to how our bird game worked. The main difference is instead of randomly
-placing the enemies, there are only two locations, ground or air.*/
-
 // http://pixijs.download/v4.4.0/docs/index.html
 
-/** lanes
- * 1
- * 2
- * 3  (430, 350)
- * 4
- * 5
+/** lane (x, y) from left to right
+ * add 170 starting from left to right
+ * 1 (100, 350)
+ * 2 (270, 350)
+ * 3 (440, 350)
+ * 4 (510, 350)
+ * 5 (780, 350)
  * 
 */
 
 export default class Model {
-
-    
+ 
     constructor() {
 
-        
+        // Numbers for determining game and background size based on screen
         const scaleWidth = screen.width * (7/12) ;
         const scaleHeight = screen.height * (7/12) ;
         const piecesWidth = screen.width * (1/14);
         const piecesHeight = screen.height * (2/14);
 
-        
-        let app = new PIXI.Application({width: scaleWidth , height: scaleHeight});
 
-        document.getElementById('root').appendChild(app.view);
+        // creates the pixi app 
+        this.app = new PIXI.Application({width: scaleWidth , height: scaleHeight});
 
+
+        //sets up the background and adds it to the stage
         let background = new PIXI.Sprite.from("../images/background.png");
         background.height = scaleHeight;
         background.width = scaleWidth;
+        this.app.stage.addChild(background);
 
-        app.stage.addChild(background);
 
-
+        //creates the player and adds it to the stage
         this.player = {
             sprite: new PIXI.Sprite.from("../images/ram.png"),
             isDead:false, 
-        }
-        console.log(this.player)
-        this.player.sprite.x = 450; //place holder values, i doubt (0,0) will work
+        };
+        this.player.sprite.x = 440; //place holder values, i doubt (0,0) will work
         this.player.sprite.y = 350; 
         this.player.sprite.width = piecesWidth;
         this.player.sprite.height = piecesHeight ;
-
-        app.stage.addChild(this.player.sprite);
+        this.app.stage.addChild(this.player.sprite);
 
 
 
@@ -57,29 +51,29 @@ export default class Model {
         //when they move off the screen, they are removed from this array
         // an array of obstacle objects 
         let obstacleArray = [] ; 
-
-        //basically going to make the whole game in here and append it using the view
-
         
     }
+
     //this will be used by the view to append the app to the dom
     getApp() {
         return this.app;
     }
 
+    //moves the user to the left
     left (){
-        this.player.sprite.x-= 10 ;
-        console.log( "(" + this.player.sprite.x  + ", " + this.player.sprite.y + ")")
+        if (this.player.sprite.x !== 100) {
+            this.player.sprite.x-= 170 ;
+        }
     }
+    //moves the user to the right
     right() {
-        this.player.sprite.x+= 10 ;
-        console.log( "(" + this.player.sprite.x  + ", " + this.player.sprite.y + ")")
-
-        //lower the player's y postion
-        // different sprite image? 
-        // does it duck for a second then go up or on key release (hold to duck)? 
+        if (this.player.sprite.x !== 780){
+            this.player.sprite.x+= 170 ;
+        }
     }
 
+    //checks to see if an enemy has intersected with the player
+    // if true, then the game will end
     collision() {
         // check to see if any obstacles from array intersected with the player
         // set the player to Dead if this is true
@@ -141,6 +135,9 @@ export default class Model {
 
 class obstacle {
     constructor() {
+        sprite= new PIXI.Sprite.from("../images/devil.png");
+        this.sprite.x = 0; 
+        this.sprite.y = 0; 
 
     }
     move(){
@@ -150,22 +147,7 @@ class obstacle {
     // functions to handle the dimensions of the obstacle
 
 }
-class flyinyObstactle extends obstacle {
-    constructor() {
-        sprite= new PIXI.Sprite.from("../images/flyingOb.png");
-        this.sprite.x = 0; 
-        this.sprite.y = 0; 
-    }
-    
-}
 
-class groundObstactle extends obstacle {
-    constructor() {
-        sprite= new PIXI.Sprite.from("../images/groundOb.png");
-        this.sprite.x = 0;
-        this.sprite.x = 0; 
-    }
-}
 
 Model.Event = {
     LOSE: 1
--- engine/view.js
+++ engine/view.js
@@ -3,12 +3,64 @@
 export default class View {
     constructor(model) {
         this.model = model ;        
-        
-        //game should be made in the model
-        //we will append the pixi thing in here
+        const $root =  document.getElementById("root");
 
-       // document.getElementById("root").appendChild(this.model.getApp());   
+        //WANNA GET RID OF LANDING PAGE FOR TESTING???
+        //comment out these three lines below!!
+        let landing = this.landingPage() ;
+        $root.appendChild(landing) ;
+        document.getElementById("start").addEventListener("click", this.startGame);
 
+        //THEN
+        //uncomment this line
+    //$root.appendChild(this.model.getApp().view);   
+
+
+        /**IDEA
+         * we made a landing page that loads initial that says "play" button
+         * then when the button is pressed, we used .replaceChild 
+         * to replace the start screen with the game screen
+         * 
+         * this also allows us to replace the game screen with a loss screen
+         * this loss screen will also display the top 10 leaderboard
+         */
+
+    }
+
+    startGame = () => {
+        //overall id tag
+        let page = document.createElement("article");
+        page.setAttribute("id", "game");
+
+        //gets the app from the model
+        //must be an arrow function or this will cause undefined errors
+        let app = this.model.getApp().view ;
+        page.appendChild(app);
+
+        //replaces the landing page with the game
+        document.getElementById("landingPage").replaceWith(page);   
+    }
+
+    landingPage() {
+        //creates the overall id tag
+        let page = document.createElement("article");
+        page.setAttribute("id", "landingPage");
+
+        //first content div tag, everything lives in here
+        let content = document.createElement("div") ;
+        content.setAttribute("class", "");
+
+
+        //start button
+        let button = document.createElement("button");
+        button.setAttribute("class", "button");
+        button.setAttribute("id", "start");
+        button.innerHTML = "Start Game!";
+
+        //adds all contents to one item and returns the item
+        content.appendChild(button) ;
+        page.appendChild(content);
+        return page;
 
     }
 }
