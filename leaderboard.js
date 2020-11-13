e346e67584522dbd0f16aadce7f26ed3285a78de
wbucher3 <wbucher@live.unc.edu>
2020-11-13 01:10:55 -0500

    added some boiler code for the leaderboard

--- leaderboard.js
+++ leaderboard.js
@@ -1,5 +1,41 @@
+/**this view assumes each entry has the attributes 
+ *  object {
+ *      id: number, (just numerate like they did for twitter, idk if we need this)
+ *      name: string,
+ *      score: number,
+ *      }
+ * 
+ * maybe a date field???
+ */
+
 //gets the data from the backend
 const getData = function() {
+    //return an array of data objects 
+}
+
+//rough draft of what rendering the leaderboard might be
+const renderTable = function(data) {
+    let page = document.createElement("div");
+    page.setAttribute("class", "content");
+    page.setAttribute("id", "table");
+
+    let table = document.createElement("table"); 
+
+    for (let i = 0 ; i < 50 ; i++) {
+        let overall = document.createElement("tr");
+
+        let name = document.createElement("td");
+        name.innerText = data[i].name;
+
+        let score = document.createElement("td");
+        score.innerText = data[i].score ; 
+
+        overall.appendChild(name);
+        overall.appendChild(score);
+
+        table.appendChild(overall);
+
+    }
 
 }
  
@@ -7,7 +43,8 @@ const getData = function() {
 //loads everything in
 const domLoader = function() {
     let $root = document.getElementById("root") ; 
-
+    let dataArray = getData() ;
+    let table = renderTable(dataArray) ;
 }
 //kicks off the webpage
 domLoader();
