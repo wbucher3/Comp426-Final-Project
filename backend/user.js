43509fea17b13d1fa26c8474e6a4b3d027484e3d
Rebekah Seawell <seawellr@live.unc.edu>
2020-11-21 20:53:09 -0500

    signup almost done

--- backend/user.js
+++ backend/user.js
@@ -1,40 +1,22 @@
-const userData = require('data-store')({path: process.cwd() + '/data/users.json'});
+const { login } = require('../APIcalls');
+
+const loginData = require('data-store')({path: process.cwd() + '/data/users.json'});
 
 class User {
 
-    constructor(username, password) {
-        this.username = username.toString();
+    constructor(password) {
         this.password = password;
+    }
+
+    delete(user) {
+        loginData.del(user);
     }
 
 }
 
-// User.getAllIDs = () => {
-//     return Object.keys(userData.data).map((id => {return parseInt(id);}));
-// };
-
-// User.getUserByTerm = (term) => {
-//     return Object.keys(userData.data).filter(id => {
-
-//         let user = userData.get(id).user;
-//         if (user.includes(term)) {
-//             return id;
-//         }
-//     }).map(id => userData.get(id));
-// }
-
-// User.findByID = (id) => {
-//     let sdata = userData.get(id);
-//     if (sdata != null) {
-//         return new User(sdata.id, sdata.user, sdata.score);
-//     } else {
-//         return null;
-//     }
-// };
-
-User.create = (user, score) => {
-    let s = new User(user, score);
-    userData.set(s.username, s);
+User.create = (user, password) => {
+    let s = new User(password);
+    loginData.set(user, s);
     return s;
 };
 
